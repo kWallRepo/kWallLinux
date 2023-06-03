f44607a44c258cf4dda41900a5802cf12f60cd09
K0 <109837094+TotallyNotK0@users.noreply.github.com>
2023-06-03 00:06:56 -0500

    Security patch
    Fixed some possible security vulnerabilities

--- kWall.py
+++ kWall.py
@@ -2,6 +2,8 @@ import requests
 import csv
 import subprocess
 from io import StringIO
+import re
+import shlex
 
 # Fetch the IP blocklist
 response = requests.get("https://feodotracker.abuse.ch/downloads/ipblocklist.csv").text
@@ -18,24 +20,26 @@ else:
     for row in csv_data:
         if ip_index < len(row):
             ip = row[ip_index]
-            if ip:
+            if ip and re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', ip):
                 # Check if rule already exists
-                check_rule = f"sudo ufw show added | grep -w {ip}"
-                existing_rule = subprocess.run(["bash", "-c", check_rule], capture_output=True, text=True)
+                check_rule = ["ufw", "show", "added", "|", "grep", "-w", ip]
+                existing_rule = subprocess.run(check_rule, capture_output=True, text=True)
                 if existing_rule.stdout.strip():
                     print("Rule already exists. Skipping:", ip)
                 else:
                     print("Added Rule to block:", ip)
                     # Add firewall rule to block IP
-                    rule = f"sudo ufw deny from {ip}"
-                    subprocess.run(["bash", "-c", rule], check=True)
+                    rule = ["sudo", "ufw", "deny", "from", ip]
+                    subprocess.run(rule, check=True)
                     added_rules.append(ip)
+            else:
+                print("Invalid IP address:", ip)
         else:
             print("Invalid row:", row)
 
     # Verify added rules
-    verify_rule = "sudo ufw show added"
-    subprocess.run(["bash", "-c", verify_rule])
+    verify_rule = ["ufw", "show", "added"]
+    subprocess.run(verify_rule)
 
     # Save added rules to a file
     with open("added_rules.txt", "w") as file:
